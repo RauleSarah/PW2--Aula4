b36abd40275b689e7fad93a4b2d652b72d617bac
Sarah Cristina <sarah.brina@hotmail.com>
2019-08-28 20:15:39 -0300

    Inicialização Da Classe Aluno

---
+++ aluno.ts
@@ -0,0 +1,18 @@
+ class Aluno{
+ //nome : String;
+
+ constructor( public nome:String){
+     this.nome = nome;
+    }
+
+    exibir(){
+        console.log("Nome" + this.nome)
+    }
+
+
+ 
+}
+
+/*let Rogerio = new Aluno ("Rogerio de Morais"){
+    Rogerio.nome= "Rogerio e Morais";
+} */
---
+++ dist/aluno.js
@@ -0,0 +1,14 @@
+var Aluno = /** @class */ (function () {
+    //nome : String;
+    function Aluno(nome) {
+        this.nome = nome;
+        this.nome = nome;
+    }
+    Aluno.prototype.exibir = function () {
+        console.log("Nome" + this.nome);
+    };
+    return Aluno;
+}());
+/*let Rogerio = new Aluno ("Rogerio de Morais"){
+    Rogerio.nome= "Rogerio e Morais";
+} */ 
